e2d24b2727b002f8a9ea2d6486466561b1f16229
Quinn <quinnvoker@gmail.com>
2020-07-31 17:34:55 +0000

    now using methods

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-// Arrays to keep track of each task's state
-const taskTitles = [];
-const taskComplete = [];
-const taskDescription = [];
-
 // Create a new task by adding to the arrays
 // A new task will be created as incomplete
 const newTask = function(title, description) {
@@ -10,6 +5,12 @@ const newTask = function(title, description) {
     title,
     description,
     complete: false,
+    logState: function() {
+      console.log(`${this.title} has ${this.complete ? "" : "not "}been completed`);
+    },
+    markCompleted: function() {
+      this.complete = true;
+    }
   };
   return task;
 };
@@ -19,9 +20,9 @@ const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter b
 const task2 = newTask("Do Laundry", "😨");
 const tasks = [task1, task2];
 
-// for now, let's just make sure we see our tasks
-console.log(tasks);
-
+task1.logState(); // Clean Cat Litter has not been completed
+task1.markCompleted();
+task1.logState(); // Clean Cat Litter has been completed
 // // Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
 // function completeTask(taskIndex) {
 //   taskComplete[taskIndex] = true;
